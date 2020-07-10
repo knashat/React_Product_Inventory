ce77d40bf43e06258648afa7ef1eede1a39391c8
knashat <khalid.nashaat@gmail.com>
2020-07-11 01:59:05 +0200

    Axios

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import AddProductForm from "./Forms/AddProductForm";
 import EditQuantityForm from "./Forms/EditQuantityForm";
 import DeleteProductForm from "./Forms/DeleteProductForm";
@@ -6,30 +7,56 @@ import ListProduct from "./ListProduct/ListProduct";
 
 const Products = () => {
   const [productList, setProductList] = useState([]);
+  useEffect(()=> {
+    axios.get("http://localhost:3000/products")
+    .then(res => setProductList(res.data));
+  }, []);
+
+  
 
   const addProduct = product => {
     setProductList([...productList, product]);
+    axios.post("http://localhost:3000/products", product)
+    //.then(res => console.log(res))
   };
 
   const editQuantity = (selectEditProduct, productNewQuantity) => {
     setProductList(
       productList.map(product => {
         if (product.name === selectEditProduct) {
+          axios.put(`http://localhost:3000/products/${product.id}`, {...product, quantity: productNewQuantity})
+          //.then(res => console.log(res));
+          
           return {
             ...product,
             quantity: productNewQuantity
-          };
+          }       
+        
         } else {
           return product;
         }
+        
       })
     );
+    
   };
 
   const deleteProduct = selectDeleteProdcut => {
-    setProductList(
-      productList.filter(product => product.name !== selectDeleteProdcut)
-    );
+    productList.map(product => {
+      if (product.name === selectDeleteProdcut){
+        axios.delete(`http://localhost:3000/products/${product.id}`)
+         // .then(res => console.log(res));
+         return setProductList(
+            productList.filter(product => product.name !== selectDeleteProdcut)
+          );
+      }
+      else {
+        return product;
+      }
+    });
+    
+    
+    
   };
 
   return (
